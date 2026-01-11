15f8f1a24ed2d891c3d0bded09fd3a288cba3439
Mr Proton <251812551+ofx-proton@users.noreply.github.com>
2026-01-11 15:02:03 +0545

    Now adding background , added logic to die and some minor changes

--- scripts/playertscn.gd
+++ scripts/playertscn.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -180.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
+
+var is_dead := false
 
 func _physics_process(delta):
+	if is_dead:
+		return
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -18,9 +23,19 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-	# Kill on ANY collision
+	# Kill on any collision
 	if get_slide_collision_count() > 0:
 		die()
 
 func die():
+	if is_dead:
+		return
+
+	is_dead = true
+	print("dead")
+	# stop movement
+	
+	# wait 3 seconds
+	await get_tree().create_timer(3.0).timeout
+
 	get_tree().reload_current_scene()
